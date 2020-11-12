724dd4ab917facd40662c6dd9a815aa767be1db6
DumiduPramith <dumidu42@gmail.com>
2020-11-12 22:17:58 +0530

    modify zip extracter

--- installer.py
+++ installer.py
@@ -4,8 +4,13 @@ def extract_zip():
     dir = '/etc/main.zip'
     target_dir = '/etc/workon'
     if os.path.isfile(dir):
-        with zipfile.ZipFile(dir,"r") as zip_ref:
-            zip_ref.extractall(target_dir)
+        with zipfile.ZipFile(dir) as zip:
+            for zip_info in zip.infolist():
+    	        if zip_info.filename[-1] == '/':
+    		        continue
+    	        zip_info.filename = os.path.basename(zip_info.filename)
+    	        zip.extract(zip_info,target_dir)
+        print("Files Extract Successfuly")
     else:
         print('Zip File Not Found')
         os._exit(1)
